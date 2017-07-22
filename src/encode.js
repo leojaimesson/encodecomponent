99daaa47d4d1bc5c7621f94181b957f14c5b0be5
leojaimesson <leo.jaimesson.dev@gmail.com>
2017-07-22 17:46:09 -0300

    comentarios adicionados e pequena refatoração de nomeclaturas

--- src/encode.js
+++ src/encode.js
@@ -13,15 +13,37 @@
 
 })(this, function(){
 	'use strict'
- 
+	
+	/**
+	 * @description 
+	 * Verifica se um elemento realmente é um objeto.
+	 * 
+	 * @param {Object} object 
+	 * @return {Boolean}
+	 */
 	function isObject(object) {
 		return Object.prototype.toString.call(object) === '[object Object]' || typeof(object) === 'object';
 	}
 
-	function encodeURL(value) {
+	/**
+	 * @description
+	 * Recebe uma string e retorna a sua versão em formato encode
+	 * 
+	 * @param {String} value
+	 * @return {String} 
+	 */
+	function encodeURI(value) {
 		return encodeURIComponent(value);
 	}
 
+	/**
+	 * @description
+	 * Converte um sub-objeto em seu respectivo uriComponent
+	 * 
+	 * @param {String} prefix 
+	 * @param {Object} object 
+	 * @return {String}
+	 */
 	function _buildCodeSubObjects(prefix, object) {
 		var prefixList = prefix.split(',');
 		return Object.keys(object).reduce(function(acc, item) {
@@ -29,20 +51,27 @@
 	 			return acc + _buildCodeSubObjects((prefix + ',' + item) , object[item]);
 			}
 	 		var pre = prefixList.reduce(function(acc , item) {
-	 			return acc.length > 0 ? acc + encodeURL(item) + encodeURL(']') + encodeURL('[') : acc + encodeURL(item) + encodeURL('[');
+	 			return acc.length > 0 ? acc + encodeURI(item) + encodeURI(']') + encodeURI('[') : acc + encodeURI(item) + encodeURI('[');
 	 		}, '')
 
-			return acc + pre + encodeURL(item) + encodeURL(']') + '=' + encodeURL(object[item]) +'&';
+			return acc + pre + encodeURI(item) + encodeURI(']') + '=' + encodeURI(object[item]) +'&';
 		},'');
 	}
 
+	/**
+	 * @description
+	 * Converte um objeto em um uriComponet
+	 * 
+	 * @param {Object} object 
+	 * @return {String}
+	 */
 	function _buildCode(object) {
 			return Object.keys(object).reduce(function(prev, item){
 				if(isObject(object[item])){
 					var result = _buildCodeSubObjects(item, object[item]).split('');
 					return prev + '&' + result.slice(0, result.length-1).join('');
 				}
-				return (!prev ? '' : prev + '&') + encodeURL(item) + '=' + encodeURL(object[item]);
+				return (!prev ? '' : prev + '&') + encodeURI(item) + '=' + encodeURI(object[item]);
 			}, '');
 		}
 
